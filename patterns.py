cbe41ab72f8d7f6800c775e14524ad3c29e0481b
M-Usman-Nadeem <usmanprg9@gmail.com>
2023-08-22 17:23:40 +0500

    first pattern

--- patterns.py
+++ patterns.py
@@ -1,1 +1,11 @@
-print('python')
+# pattern 1
+star_pattern_one=""
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        star_pattern_one+="*"
+    if(i!=(n-1)):
+     star_pattern_one+="\n"    
+
+print(star_pattern_one)    
+ 
