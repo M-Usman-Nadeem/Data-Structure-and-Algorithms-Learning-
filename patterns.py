a0bdda268cbe036cfa4d91a29463743ab625fb69
M-Usman-Nadeem <usmanprg9@gmail.com>
2023-08-22 18:37:40 +0500

    patterns

--- patterns.py
+++ patterns.py
@@ -1,11 +1,29 @@
 # pattern 1
-star_pattern_one=""
-n=int(input())
-for i in range(n):
-    for j in range(n):
-        star_pattern_one+="*"
-    if(i!=(n-1)):
-     star_pattern_one+="\n"    
+# star_pattern_one=""
+n = int(input())
+# for i in range(n):
+#     for j in range(n):
+#         star_pattern_one+="*"
+#     if(i!=(n-1)):
+#      star_pattern_one+="\n"
 
-print(star_pattern_one)    
- 
+# print(star_pattern_one)
+# star_pattern_two
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+# print(end="\n")
+# star_pattern_three
+# for i in range(1, n+1):
+#     for j in range(0, n-i):
+#         print(" ", end="")
+#     for k in range(1, i*2):
+#         print("*", end="")
+#     print(end="\n")
+# star_pattern_four
+for i in range(n,0,-1):
+    for j in range(0, n-i):
+        print(" ", end="")
+    for k in range(1, i*2):
+        print("*", end="")
+    print(end="\n")
